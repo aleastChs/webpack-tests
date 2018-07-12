86962442d3600109bd93fc0be52397e9f50e9b62
aleastChs <aleast@student.chalmers.se>
2018-07-12 12:54:21 +0200

    Added image within JavaScript module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
 
 function component() {
     var element = document.createElement('div');
     
     element.innerHTML = _.join(['Hello', "Webpack"], ' ')
     element.classList.add('hello');
+    
+    var logo = new Image();
+    logo.src = Icon;
+    
+    element.appendChild(logo);
     
     return element;
 }
